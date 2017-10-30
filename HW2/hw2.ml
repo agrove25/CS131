1ea6234ea4fd5baf08ce22810e22bf56cc8b6608
agrove25 <koeswantoandrew@gmail.com>
2017-10-29 22:38:46 -0700

    10/29/2017

--- HW2/hw2.ml
+++ HW2/hw2.ml
@@ -10,32 +10,6 @@ let rec prod_fun nt rules = match rules with
 
 let convert_grammar (start, rules) = (start, fun nt -> (prod_fun nt rules));; 
 
-(*)
-let rec matcher gram nt acc der frag =
-    let rec check_rhs gram rhs acc der frag = match rhs with
-      | [] -> acc der frag
-      | (N x) :: n_tail -> (matcher gram x (check_rhs gram n_tail acc) der frag)
-      | (T y) :: t_tail -> match frag with
-        | [] -> None
-        | h :: t -> if (h = y)
-                    then (check_rhs gram t_tail acc der t)
-                    else None
-    in
-      let rec check_rest gram nt rhs acc der frag = match rhs with
-      | [] -> None
-      | h :: t -> match (check_rhs gram h acc (der@[(nt,h)]) frag) with
-        | Some(a, b) -> Some(a, b)
-        | None -> (check_rest gram nt t acc der frag)
-    
-    in
-
-    (check_rest gram nt (gram nt) acc der frag)
-
-let rec parse_prefix (symbol, gram) acc frag =
-  (matcher gram symbol acc [] frag)
-*)
-
-
 let rec matcher nt rules alt_list acc d frag =
 
   let rec checker rhs d frag = match rhs with
